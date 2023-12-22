e0cba17c843d827b1532f33dab5c739b34a7c467
wnsgur364 <wnsgur364@naver.com>
2023-12-22 09:57:56 +0900

    vue2 프로젝트와 스프링 부트 연동

---
+++ vue2Front/src/views/AboutView.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref } from "vue";
+import axios from "axios";
+
+const str = ref("");
+
+// axios로 Spring Boot에 GET request를 보냄
+axios
+  .get("/api/about")
+  .then((res) => {
+    console.log(res.data);
+    console.log(res.data.str);
+    str.value = res.data.str;
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+</script>
+
+<template>
+  <div class="about">
+    <h1>{{ str }}</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
